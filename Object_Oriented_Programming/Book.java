149dbd8f99f2ba03fe88a34d329f5296a8e7cd9f
sudhanshupathak <pji101329@gmail.com>
2025-08-07 10:00:00 +0530

    [sudhanshu] add . Book added

---
+++ Object_Oriented_Programming/Book.java
@@ -0,0 +1,22 @@
+class Book {
+    String title;
+    String author;
+    double price;
+
+    Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    void displayBookDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        Book book = new Book("Java Programming", "James Gosling", 799);
+        book.displayBookDetails();
+    }
+}
