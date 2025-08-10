ded1c116fb8a0baac04524c22dc98b030f4ce07f
sudhanshupathak <pji101329@gmail.com>
2025-08-10 10:00:00 +0530

    [sudhanshu] add . jobRole  added

---
+++ Day01_JavaGenerics/AIDrivenResumeSystem/JobRole.java
@@ -0,0 +1,15 @@
+package AIDrivenResumeSystem;
+
+abstract class JobRole {
+    private String candidateName;
+
+    public JobRole(String candidateName) {
+        this.candidateName = candidateName;
+    }
+
+    public String getCandidateName() {
+        return candidateName;
+    }
+
+    public abstract void evaluate();
+}
