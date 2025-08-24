d221e13aa522b4c3b70d729b43142d1fed3f42c8
sudhanshupathak <pji101329@gmail.com>
2025-08-24 15:08:54 +0530

    area of circle

---
+++ Day1_ProgrammingElement/AreaOfCircle.java
@@ -0,0 +1,8 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        double radius = 7.0;
+        double area = Math.PI * radius * radius;
+        System.out.println("The area of the circle with radius " + radius + " is " + area);
+    }
+    
+}
